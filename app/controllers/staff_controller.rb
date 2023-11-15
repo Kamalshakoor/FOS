2ac31e70722897b73cc5782dfe86099e11e00063
kamal shakoor <kamalshakoor29022000.com>
2023-11-15 12:29:10 +0500

    Updated new staff page view

--- app/controllers/staff_controller.rb
+++ app/controllers/staff_controller.rb
@@ -1,17 +1,18 @@
 class StaffController <  ApplicationController
+    before_action :authenticate_user!
     before_action :authenticate_admin, only: [:new, :create, :index, :destroy]
   
-    # Index action to list staff members
+    # list staff members
     def index
         @staff_members = User.where(role: 'staff').order(created_at: :desc)
     end
   
-    # New action to display the staff creation form
+    # display the staff creation form
     def new
       @staff_member = User.new
     end
   
-    # Create action to handle staff creation form submission
+    # staff creation form submission
     def create
       @staff_member = User.new(staff_params)
       @staff_member.role = 'staff' # Set the role to 'staff'
@@ -24,7 +25,7 @@ class StaffController <  ApplicationController
       end
     end
   
-    # Destroy action to delete staff members
+    # delete staff members
     def destroy
       @staff_member = User.find(params[:id])
       if @staff_member.destroy
@@ -42,7 +43,7 @@ class StaffController <  ApplicationController
     end
   
     def authenticate_admin
-      # Add your authentication logic here to check if the user is an admin
+      # check if the user is an admin
       unless current_user.admin?
         flash[:alert] = "You are not authorized to perform this action."
         redirect_to root_path
